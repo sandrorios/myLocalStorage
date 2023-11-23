9ee770716da84d5ab971fd6c41b3ea97c973f595
sandrorios <sandroderios@hotmail.com>
2023-11-23 07:30:44 -0500

    modified code

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 let para = document.getElementById('demo');
+let inputField = document.getElementById('input-field');
+let btn = document.querySelector('.btn');
+let listItem = document.getElementById('ul');
 
 
-function populateStorage(){
+function populateStorage(e){
+    e.preventDefault();
+    // localStorage.setItem('name', 'John Doe');
     localStorage.setItem('name', 'John Doe');
 
-    const name = localStorage.getItem('name');
+    let name = localStorage.getItem('name');
+    
     para.appendChild(document.createTextNode(name));
+
+
+
     console.log(name);
 }
 
-populateStorage();
+btn.addEventListener('click', populateStorage);
+// listItem.addEventListener()
+// populateStorage();
