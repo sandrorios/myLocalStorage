f00a575e0fdcfd8bcfe4fee2ccfd3a25c8e47c55
sandrorios <sandroderios@hotmail.com>
2023-11-23 06:58:36 -0500

    did some changes

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+let para = document.getElementById('demo');
+
+
 function populateStorage(){
     localStorage.setItem('name', 'John Doe');
 
     const name = localStorage.getItem('name');
-
+    para.appendChild(document.createTextNode(name));
     console.log(name);
 }
 
