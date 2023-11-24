149f5c2010385f05c1a3f5726a0bb427d5ebabcb
sandrorios <sandroderios@hotmail.com>
2023-11-24 06:24:03 -0500

    added font awesom to index and created button

--- listItem/script.js
+++ listItem/script.js
@@ -15,10 +15,24 @@ function addItem(e){
     let li = document.createElement('li');
     li.appendChild(document.createTextNode(newItem));
     listItem.appendChild(li);
-
+    let button = createButton("remove-item");
+    li.appendChild(button);
     inputField.value = '';
     inputField.focus();
 }
 
+function createButton(classes) {
+    let button = document.createElement('button');
+    button.className = classes;
+    let icon = createIcon('fa-solid fa-xmark');
+    button.appendChild(icon);
+    return button;
+}
+
+function createIcon(classes){
+    let icon = document.createElement('i');
+    icon.className = classes;
+    return icon;
+}
 
 formItem.addEventListener('submit', addItem);
