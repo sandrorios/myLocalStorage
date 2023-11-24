e0b0ce69e0ea92dbdcb157bbac7ee933d911b2ee
sandrorios <sandroderios@hotmail.com>
2023-11-24 06:26:37 -0500

    created code for the delete button

--- listItem/script.js
+++ listItem/script.js
@@ -35,4 +35,11 @@ function createIcon(classes){
     return icon;
 }
 
+function removeItem(e) {
+    if(e.target.parentElement.classList.contains('remove-item')){
+        e.target.parentElement.parentElement.remove();
+    }
+}
+
+listItem.addEventListener('click', removeItem);
 formItem.addEventListener('submit', addItem);
